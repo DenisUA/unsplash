8966455bfe3397236d8e83d82b9ba6ee6f276d68
Denis <Y.Dionis@gmail.com>
2017-05-02 22:58:50 +0300

    Routes setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :photos, only: %i[show index]
+  post 'search' => 'photos#search'
+
+  root 'photos#index'
 end
