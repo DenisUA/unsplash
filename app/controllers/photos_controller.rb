08afacefe9a55d8c839eee2a66e16ea40dfeab16
Denis <Y.Dionis@gmail.com>
2017-05-02 22:55:42 +0300

    Added photos controller

---
+++ app/controllers/photos_controller.rb
@@ -0,0 +1,17 @@
+class PhotosController < ApplicationController
+  def index
+    if params[:photo]
+      response = HTTParty.get(
+          "https://api.unsplash.com/search/photos?page=1&per_page=30&query=#{params[:photo][:query]}",
+          headers: {"Authorization" => "Client-ID 491b0ab078e54dbb2e8a217a09ddacd94c89905722cfa6474fc57ea67f600b6b"}
+      )
+      @photos = response['results']
+    else
+      @photos = {}
+    end
+  end
+
+  def show
+    @photo = params[:photo]
+  end
+end
